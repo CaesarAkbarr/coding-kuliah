1ff57b0b19861bb6701759a530926d308d590f90
CaesarAkbarr <caesarakbar2006@gmail.com>
2026-04-29 07:31:38 +0700

    Add Tugas7 NetBeans project and tes.py
    Add a new NetBeans Java project Tugas7 with Ant build files and project metadata (build.xml, manifest.mf, nbproject/* including generated build-impl.xml, genfiles.properties, project.properties, project.xml). Include GUI form and source files (Mahasiswa.form/.java, MainFrame.form/.java, Tugas7.java). Add Tes/OpenCV/tes.py (prints Python version), a .gitignore entry to ignore nbproject/private, and a precompiled class tugas6/src/tugas3/Tugas3.class.

---
+++ Tugas7/src/tugas7/Tugas7.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tugas7;
+
+/**
+ *
+ * @author ROG G513RM
+ */
+public class Tugas7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
