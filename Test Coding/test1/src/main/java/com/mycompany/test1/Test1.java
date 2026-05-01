9c9463e6404ce4a34c7d294a5a81c2563b561df5
CaesarAkbarr <caesarakbar2006@gmail.com>
2026-05-01 10:21:57 +0700

    Reorganize projects under 'Kuliah Coding' & cleanup
    Move many existing project folders and files into a new 'Kuliah Coding' subdirectory (renames/moves for Tugas1..Tugas7, Tugas_lebaran, TugasAkhirV1/V2, tugas3..tugas6, etc.). Remove obsolete build outputs and NetBeans-generated/private files (deleted many build.xml, nbproject build-impl/genfiles/private entries and compiled .class artifacts). Update .gitignore to ignore test1/target and nbproject/private for TugasAkhirV1/V2. Add a new Test Coding/test1 Maven sample project (pom.xml, Java sources, FXML and CSS).

---
+++ Test Coding/test1/src/main/java/com/mycompany/test1/Test1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.test1;
+
+/**
+ *
+ * @author ROG G513RM
+ */
+public class Test1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
