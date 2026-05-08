d04d606f1ff066b1af0aceb237329a00daeb7424
CaesarAkbarr <caesarakbar2006@gmail.com>
2026-05-08 15:26:53 +0700

    Add pemdas_quiz_final NetBeans project
    Add a new NetBeans Java project 'pemdas_quiz_final' including build scripts and project metadata (build.xml, manifest.mf, nbproject/*). Add source files and GUI forms for frames (FrameBarang, FrameMaster, FramePelanggan, FramePembelian, FramePembelianDetail, FrameSupplier), plus Koneksi and Pemdas_quiz_final main class. Update .gitignore to exclude the project's nbproject/private and build output directories.

---
+++ Kuliah Coding/pemdas_quiz_final/src/pemdas_quiz_final/Koneksi.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pemdas_quiz_final;
+
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author ROG G513RM
+ */
+public class Koneksi {
+    public static Connection con;
+    public static Statement stmt;
+
+    public static Connection getKoneksi() {
+        try {
+            String url = "jdbc:mysql://localhost/db_pemdas";
+            String user = "root";
+            String pass = "";
+            con = DriverManager.getConnection(url, user, pass);
+            stmt = con.createStatement();
+            System.out.println("Koneksi berhasil dari java");
+        } catch (Exception e) {
+            System.err.println("Koneksi gagal" + e.getMessage());
+        }
+        return null;
+    }
+
+    public static void ubahData(String perintah) {
+        try {
+            String url = "jdbc:mysql://localhost/db_pemdas";
+            String user = "root";
+            String pass = "";
+            con = DriverManager.getConnection(url, user, pass);
+            stmt = con.createStatement();
+            String query = perintah;
+            stmt.executeUpdate(query);
+            System.out.println("Data berhasil diupdate");
+        } catch (Exception e) {
+            System.err.println("Data gagal diupdate " + e.getMessage());
+        }
+    }
+}
---
+++ Kuliah Coding/pemdas_quiz_final/src/pemdas_quiz_final/Pemdas_quiz_final.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pemdas_quiz_final;
+
+/**
+ *
+ * @author ROG G513RM
+ */
+public class Pemdas_quiz_final {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
